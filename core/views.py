998a80e7323c190859fa98459336d165d6b509c7
TGOSS1984 <tomgoss84@gmail.com>
2026-01-22 19:05:05 +0000

    feat(storefront): add featured items and new arrivals to homepage

--- core/views.py
+++ core/views.py
@@ -1,5 +1,23 @@
 from django.shortcuts import render
+from catalog.models import Product
+
 
 def home(request):
-    return render(request, "core/home.html")
+    featured = (
+        Product.objects.filter(is_active=True, stock_qty__gt=0)
+        .order_by("-rarity", "name")[:8]
+    )
 
+    new_arrivals = (
+        Product.objects.filter(is_active=True)
+        .order_by("-created_at")[:8]
+    )
+
+    return render(
+        request,
+        "core/home.html",
+        {
+            "featured": featured,
+            "new_arrivals": new_arrivals,
+        },
+    )
